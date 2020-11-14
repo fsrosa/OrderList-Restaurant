0f19b492cfcd8d595b7c17d49fd92ec5df8f3de3
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2020-11-14 12:32:17 -0300

    creating my first structure of pages: home + products

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'))
