cf366518b780a07cf40ba94556c7831df1c988a3
Fabrício Rosa <fabriciosilvarosa@gmail.com>
2020-11-14 18:35:21 -0300

    home v1.0

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Orders from './pages/Orders'
 import Products from './pages/Products'
 
 export default function Routes(){
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/products" exact component={Products}/>
+                <Route path="/orders" exact component={Orders}/>
             </Switch>
         </BrowserRouter>
     )
